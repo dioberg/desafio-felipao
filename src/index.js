cd45cf19fa0a249ee230bd212f8f988f4d7f20f1
dioberg <vianadiobergue@gmail.com>
2024-01-10 14:35:48 -0800

    desafio feito

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-console.log("oi")
+let nome = "davi";
+let xp = 10002;
+
+let nivel = "";
+
+if(xp <=1000){
+    nivel= "Ferro";
+}else if(xp >=1001 && xp <= 2000){
+    nivel = "Bronze"
+}else if(xp >=2001 && xp <=5000){
+    nivel = "Prata"
+}else if(xp >=5001 && xp <=7000){
+    nivel = "Ouro"
+}else if(xp >= 7001 && xp <=8000){
+    nivel = "Platina"
+}else if(xp >= 8001 && xp <=9000){
+    nivel = "Ascendente"
+}else if(xp >= 9001 && xp <= 10000){
+    nivel = "Imortal"
+}else{
+    nivel = "Radiante"
+}
+
+
+console.log("O Herói de nome " + nome + " esta no nivel de " + nivel);
